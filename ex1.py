b1757cec7c36614d4bc5a3cfec024cd506d8605b
ivanH <nigga.2023@mail.ru>
2023-03-05 05:39:48 +0300

    task 1

---
+++ ex1.py
@@ -0,0 +1,10 @@
+﻿a = int(input("enter number: "))
+b = int(input("enter degree: "))
+def degree(a,b):
+    if b == 0:
+        return 1
+    else:
+        return a * degree(a, b-1)
+
+
+print(f"result = {degree(a ,b )}")
