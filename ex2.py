566d040bc8a807aa7609b6b9c0d2bca9c2c30284
ivanH <nigga.2023@mail.ru>
2023-03-05 06:00:57 +0300

    task 2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+﻿a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+def sum(a, b):
+    if a == 0:
+        return b
+    else:
+        return sum(a-1,b+1)
+print(f"{a} + {b} = {sum(a,b)}")
